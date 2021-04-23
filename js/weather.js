1d178d95133f7c70f68a1051dc31a9bbb56ed75a
fwlsg14 <78486337+fwlsg14@users.noreply.github.com>
2021-04-24 01:08:31 +0900

    add weather-brief

--- js/weather.js
+++ js/weather.js
@@ -1,45 +1,15 @@
 //전역 상수
-
+const WEATHER_BRIEF = document.querySelector(`#weather-brief`);
+const WEATHER_DETAIL = document.querySelector(`#weather-detail`);
 //모듈 변수
 let weather = {};
 weather.currentLocation = {};
+weather.currentWeather = {};
 
 //초기 실행 함수
 weather.init = function () {
   weather.loadLocation();
   weather.getWeather();
-};
-//weather 얻기
-weather.getWeather = function () {
-  if (weather.currentLocation.latitude) {
-    console.log("weather");
-    (function () {
-      const location = weather.currentLocation;
-      const lat = location.latitude;
-      const lon = location.longitude;
-      const apiKey = `0f097166a0d965593fdf60426de7630f`;
-      fetch(
-        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
-      )
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (json) {
-          console.log(json);
-          weather.currentWeather = {
-            feelsTemp: json.main.feels_like,
-            humidity: json.main.humidity,
-            pressure: json.main.pressure,
-            temp: json.main.temp,
-            sunrise: json.sys.sunrise,
-            sunset: json.sys.sunset,
-            windDeg: json.wind.deg,
-            windSpeed: json.wind.speed,
-          };
-          console.dir(weather.currentWeather);
-        });
-    })();
-  }
 };
 
 //location 불러오기
@@ -71,6 +41,73 @@ weather.getlocation = function () {
   } else {
     console.log(`Geolocation is not supported by this browser.`);
   }
+};
+//weather 얻기
+weather.getWeather = function () {
+  if (weather.currentLocation.latitude) {
+    const location = weather.currentLocation;
+    const lat = location.latitude;
+    const lon = location.longitude;
+    const apiKey = `0f097166a0d965593fdf60426de7630f`;
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    )
+      .then((response) => {
+        return response.json();
+      }) //날씨 불러오기
+      .then((json) => {
+        console.log(json);
+        object = {
+          name: json.name,
+          feelsTemp: json.main.feels_like,
+          humidity: json.main.humidity,
+          pressure: json.main.pressure,
+          temp: json.main.temp,
+          sunrise: json.sys.sunrise,
+          sunset: json.sys.sunset,
+          windDeg: json.wind.deg,
+          windSpeed: json.wind.speed,
+          weatherMain: json.weather[0].main,
+          weatherDesc: json.weather[0].description,
+          weatherIcon: json.weather[0].icon,
+        };
+        if (json.rain) {
+          object.rain1h = json.rain[`1h`];
+          object.rain3h = json.rain[`3h`];
+        }
+        if (json.snow) {
+          object.snow1h = json.snow[`1h`];
+          object.snow3h = json.snow[`3h`];
+        }
+        return object;
+      })
+      .then((object) => {
+        //간략한 날씨 표시
+        const weatherIcon = object.weatherIcon;
+        const tempKelvin = object.temp;
+        const name = object.name;
+        console.log(weatherIcon, tempKelvin, name);
+        //element 생성
+        const img = document.createElement("img");
+        const src = document.createAttribute("src");
+        src.value = `http://openweathermap.org/img/wn/${weatherIcon}.png`;
+        img.setAttributeNode(src);
+        const tempSpan = document.createElement("span");
+        const tempCelsius = tempKelvin - 273.15;
+        tempSpan.innerText = `${tempCelsius}ºC`;
+        const location = document.createElement("p");
+        location.innerText = `${name}`;
+        //element #weather-brief에 넣기
+        WEATHER_BRIEF.appendChild(img);
+        WEATHER_BRIEF.appendChild(tempSpan);
+        WEATHER_BRIEF.appendChild(location);
+        //상세한 날씨표시
+      });
+  }
+};
+// weather-brief 클릭시 weather-detail 토글
+weather.toggleWeatherDetail = function () {
+  toggleHidden(WEATHER_DETAIL);
 };
 
 weather.init();
